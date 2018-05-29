962fd207461941f140e6865fed0eafd7623383b7
admin <admin@MIS>
2018-05-29 17:07:19 +0800

    添加项目文件。

---
+++ ServerKit.UI/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ServerKit.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_check = new System.Windows.Forms.Button();
+            this.btn_Repair = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_check
+            // 
+            this.btn_check.Location = new System.Drawing.Point(23, 33);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(110, 42);
+            this.btn_check.TabIndex = 0;
+            this.btn_check.Text = "Check now!";
+            this.btn_check.UseVisualStyleBackColor = true;
+            this.btn_check.Click += new System.EventHandler(this.btn_check_Click);
+            // 
+            // btn_Repair
+            // 
+            this.btn_Repair.Location = new System.Drawing.Point(23, 103);
+            this.btn_Repair.Name = "btn_Repair";
+            this.btn_Repair.Size = new System.Drawing.Size(110, 42);
+            this.btn_Repair.TabIndex = 0;
+            this.btn_Repair.Text = "Repair";
+            this.btn_Repair.UseVisualStyleBackColor = true;
+            this.btn_Repair.Click += new System.EventHandler(this.btn_Repair_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkedListBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btn_Repair);
+            this.splitContainer1.Panel2.Controls.Add(this.btn_check);
+            this.splitContainer1.Size = new System.Drawing.Size(811, 488);
+            this.splitContainer1.SplitterDistance = 653;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "显示隐藏文件或文件夹",
+            "取消默认共享"});
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(653, 488);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 488);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ServerKit";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_check;
+        private System.Windows.Forms.Button btn_Repair;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ ServerKit.UI/ServerKitController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerKit.UI
+{
+    class ServerKitController
+    {
+        public void check()
+        {
+            System.Windows.Forms.MessageBox.Show("Test");
+        }
+    }
+}
